b91bf3e09d08add762b9687428e8e29f91b5033f
SylvainGault <github@sylvaingault.fr>
2016-11-13 07:28:37 +0100

    Add News module
    It fetches the news from the front page of the site.
    
    Signed-off-by: SylvainGault <github@sylvaingault.fr>

--- newbiecontest-fuse.py
+++ newbiecontest-fuse.py
@@ -6,14 +6,19 @@ import sys
 import errno
 import stat
 import time
+import re
 import fuse
 import requests
+import lxml.html
 
 
 fuse.fuse_python_api = (0, 2)
 
 
 class AuthException(BaseException):
+    pass
+
+class ParsingException(BaseException):
     pass
 
 
@@ -70,8 +75,117 @@ class File(object):
         return self._content
 
 
-# TODO
-# Modules: news, challenges, shoutbox
+class News(object):
+    newspath = "/news"
+    urlnews = "index.php?page=news"
+    newslife = 60
+    datere = re.compile('^(\d+ \d+ \d+ à \d+:\d+:\d+)')
+
+
+    def __init__(self, req):
+        self.req = req
+        self.newslist = None
+        self.newsexpir = None
+
+
+    def handledpath(self):
+        return [self.newspath]
+
+
+    def _getnews(self):
+        now = time.time()
+        if self.newslist is not None and self.newsexpir > now:
+            return
+
+        res = self.req.get(self.urlnews)
+        doc = lxml.html.fromstring(res.content, base_url = res.url)
+        elements = doc.cssselect('div#content > div.textpad > *')
+
+        monthdict = {
+                "Janvier" : "01", "Février" : "02", "Mars" : "03",
+                "Avril" : "04", "Mai" : "05", "Juin" : "06", "Juillet" : "07",
+                "Août" : "08", "Septembre" : "09", "Octobre" : "10",
+                "Novembre" : "11", "Décembre" : "12"
+        }
+        self.newslist = {}
+
+        for i in range(0, len(elements), 4):
+            # The list end with a single <p>
+            if i + 4 > len(elements):
+                break
+
+            [title, content, foot, hr] = elements[i:i+4]
+            if title.tag != 'h2' or hr.tag != 'hr':
+                raise ParsingException()
+
+            # Build a File object
+            titletext = title.text
+            titletext = titletext.strip().replace('/', '_')
+            news = File(titletext, isWritable = False)
+
+            # Try to render the html
+            try:
+                import html2text
+                htmlcontent = lxml.html.tostring(content, method = 'html')
+                news.content = html2text.html2text(htmlcontent).encode('utf-8')
+            except ImportError:
+                news.content = lxml.html.tostring(content, encoding = 'utf-8', method = 'text')
+
+
+            # Parse the publish date of the news and set it as the File's stat
+            date = lxml.html.tostring(foot, encoding = 'utf-8', method = 'text')
+            for a, n in monthdict.items():
+                date = date.replace(a, n)
+
+            match = self.datere.match(date)
+            if match is None:
+                raise ParsingException()
+
+            date = match.group(1)
+            date = time.strptime(date, "%d %m %Y à %H:%M:%S")
+            news.stat.st_mtime = time.mktime(date)
+            news.stat.st_ctime = news.stat.st_mtime
+
+            # Add the File to the list
+            self.newslist[self.newspath + "/" + news.name] = news
+        self.newsexpir = now + self.newslife
+
+
+    def getattr(self, path):
+        self._getnews()
+        if path == self.newspath:
+            return DirStat()
+        elif path in self.newslist:
+            return self.newslist[path].stat
+        else:
+            return -errno.ENOENT
+
+
+    def readdir(self, path, offset):
+        self._getnews()
+
+        yield fuse.Direntry(".")
+        yield fuse.Direntry("..")
+        for f in self.newslist.values():
+            yield fuse.Direntry(f.name)
+
+
+    def open(self, path, flags):
+        accmode = os.O_RDONLY | os.O_WRONLY | os.O_RDWR
+        if (flags & accmode) != os.O_RDONLY:
+            return -errno.EACCES
+
+        self._getnews()
+
+        if path not in self.newslist:
+            # Should not happen because fuse always call getattr first
+            return -errno.ENOENT
+
+
+    def read(self, path, size, offset):
+        self._getnews()
+        return self.newslist[path].content[offset:offset+size]
+
 
 
 class Requests(object):
@@ -266,7 +380,8 @@ def main():
     usage = "Newbiecontest File System\n\n"
     usage += NewbiecontestFS.fusage
 
-    modules = [Requests()]
+    req = Requests()
+    modules = [req, News(req)]
     server = NewbiecontestFS(modules, usage = usage)
     server.parse(errex = 1)
     server.main()
