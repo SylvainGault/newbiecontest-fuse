06ea18dda3c00122ed3d592c95d6a7f4fa9be43e
SylvainGault <github@sylvaingault.fr>
2016-11-18 00:00:53 +0100

    FSModule: Add an empty module
    Other modules will inherit from it, thus factoring the common work.
    
    Signed-off-by: SylvainGault <github@sylvaingault.fr>

--- modules/__init__.py
+++ modules/__init__.py
@@ -2,3 +2,33 @@
 
 class ParsingException(BaseException):
     pass
+
+
+
+class FSModule(object):
+    """The empty module.
+    It usually return -ENOENT, 0 or []"""
+
+    def __init__(self):
+        pass
+
+    def getndirs(self):
+        return 0
+
+    def getattr(self, path):
+        return -errno.ENOENT
+
+    def readdir(self, path, offset):
+        return []
+
+    def open(self, path, *args, **kwargs):
+        return -errno.ENOENT
+
+    def read(self, path, *args, **kwargs):
+        return -errno.ENOENT
+
+    def write(self, path, *args, **kwargs):
+        return -errno.ENOENT
+
+    def truncate(self, path, *args, **kwargs):
+        return -errno.ENOENT
