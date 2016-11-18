0c217e88fd22b85b90fe0ea54a13a943c2f6c55a
SylvainGault <github@sylvaingault.fr>
2016-11-18 01:01:51 +0100

    NewbiecontestFS: export most code to FSSubModule
    The new class FSSubModule handles submodules in directories and a
    rootmodule whose files are directly put in the directory the modules
    lies in.
    
    Signed-off-by: SylvainGault <github@sylvaingault.fr>

--- modules/__init__.py
+++ modules/__init__.py
@@ -1,5 +1,13 @@
 # coding: utf-8
 
+import errno
+import fuse
+import itertools
+
+import fileobjects as fo
+
+
+
 class ParsingException(BaseException):
     pass
 
@@ -32,3 +40,92 @@ class FSModule(object):
 
     def truncate(self, path, *args, **kwargs):
         return -errno.ENOENT
+
+
+
+class FSSubModule(FSModule):
+    """This module can have submodules and a rootmodule. The rootmodule will
+    have its files directly on the level of the instance module. The dirmodules
+    will have their files inside a subdirectory.
+
+    Attributes:
+        rootmodule   The module that has its files in this level.
+        dirmodules   A dict that associate a directory name with a module."""
+
+    def __init__(self, rootmodule = None, dirmodules = {}, *args, **kwargs):
+        super(FSSubModule, self).__init__(*args, **kwargs)
+
+        self.rootmodule = rootmodule
+        self.dirmodules = dirmodules
+
+        if self.rootmodule is None:
+            self.rootmodule = FSModule()
+
+
+    @staticmethod
+    def pathsplit(path):
+        idx = path.find("/")
+        if idx == -1:
+            idx = len(path)
+        return (path[:idx], path[idx+1:])
+
+
+    def modulepath(self, path):
+        (prefix, tail) = self.pathsplit(path)
+        if prefix in self.dirmodules:
+            return (self.dirmodules[prefix], tail)
+        return (self.rootmodule, path)
+
+
+    def getattr(self, path):
+        (m, tail) = self.modulepath(path)
+
+        if tail == "":
+            # Asking for / or a dirmodule
+            st = fo.DirStat()
+            st.st_nlink += m.getndirs()
+
+            if path == "":
+                st.st_nlink += len(self.dirmodules)
+            return st
+
+        else:
+            # Asking for a module's content
+            return m.getattr(tail)
+
+
+    def readdir(self, path, offset):
+        (m, tail) = self.modulepath(path)
+
+        if tail == "":
+            f = [m.readdir(tail, offset)]
+            if path == "":
+                f.append(fuse.Direntry(name) for name in self.dirmodules.keys())
+            return itertools.chain(*f)
+
+        else:
+            return m.readdir(tail, offset)
+
+
+    def open(self, path, *args, **kwargs):
+        (m, tail) = self.modulepath(path)
+
+        if tail == "":
+            return -errno.EISDIR
+
+        return m.open(tail, *args, **kwargs)
+
+
+    def read(self, path, *args, **kwargs):
+        (m, tail) = self.modulepath(path)
+        return m.read(tail, *args, **kwargs)
+
+
+    def write(self, path, *args, **kwargs):
+        (m, tail) = self.modulepath(path)
+        return m.write(tail, *args, **kwargs)
+
+
+    def truncate(self, path, *args, **kwargs):
+        (m, tail) = self.modulepath(path)
+        return m.truncate(tail, *args, **kwargs)
--- newbiecontest-fuse.py
+++ newbiecontest-fuse.py
@@ -1,11 +1,10 @@
 #!/usr/bin/env python
 # coding: utf-8
 
-import errno
 import fuse
 import itertools
 
-import fileobjects as fo
+import modules
 import modules.news as news
 import modules.challenges as challenges
 import modules.authrequests as authrequests
@@ -14,93 +13,46 @@ fuse.fuse_python_api = (0, 2)
 
 
 
+
 class NewbiecontestFS(fuse.Fuse):
     def __init__(self, *args, **kwargs):
         super(NewbiecontestFS, self).__init__(*args, **kwargs)
 
         req = authrequests.AuthRequests()
+        rootmodule = authrequests.Auth(req)
 
-        self.rootmodule = authrequests.Auth(req)
+        dirmodules = {}
+        dirmodules["news"] = news.News(req)
+        dirmodules["challenges"] = challenges.Challenges(req)
 
-        self.dirmodules = {}
-        self.dirmodules["news"] = news.News(req)
-        self.dirmodules["challenges"] = challenges.Challenges(req)
-
-
-    @staticmethod
-    def pathsplit(path):
-        idx = path.find("/")
-        if idx == -1:
-            idx = len(path)
-        return (path[:idx], path[idx+1:])
-
-
-    def modulepath(self, path):
-        (prefix, tail) = self.pathsplit(path)
-        if prefix in self.dirmodules:
-            return (self.dirmodules[prefix], tail)
-        return (self.rootmodule, path)
+        self.rootfsmodule = modules.FSSubModule(rootmodule, dirmodules)
 
 
     def getattr(self, path):
         path = path[1:]
-        (m, tail) = self.modulepath(path)
-
-        if tail == "":
-            # Asking for / or a dirmodule
-            st = fo.DirStat()
-            st.st_nlink += m.getndirs()
-
-            if path == "":
-                st.st_nlink += len(self.dirmodules)
-            return st
-
-        else:
-            # Asking for a module's content
-            return m.getattr(tail)
-
+        return self.rootfsmodule.getattr(path)
 
     def readdir(self, path, offset):
-        dotdot = [fuse.Direntry("."), fuse.Direntry("..")]
         path = path[1:]
-        (m, tail) = self.modulepath(path)
-
-        if tail == "":
-            f = [m.readdir(tail, offset)]
-            if path == "":
-                f.append(fuse.Direntry(name) for name in self.dirmodules.keys())
-            return itertools.chain(dotdot, *f)
-
-        else:
-            return m.readdir(tail, offset)
-
+        dotdot = [fuse.Direntry("."), fuse.Direntry("..")]
+        f = self.rootfsmodule.readdir(path, offset)
+        return itertools.chain(dotdot, f)
 
     def open(self, path, *args, **kwargs):
         path = path[1:]
-        (m, tail) = self.modulepath(path)
-
-        if tail == "":
-            return -errno.EISDIR
-
-        return m.open(tail, *args, **kwargs)
-
+        return self.rootfsmodule.open(path, *args, **kwargs)
 
     def read(self, path, *args, **kwargs):
         path = path[1:]
-        (m, tail) = self.modulepath(path)
-        return m.read(tail, *args, **kwargs)
-
+        return self.rootfsmodule.read(path, *args, **kwargs)
 
     def write(self, path, *args, **kwargs):
         path = path[1:]
-        (m, tail) = self.modulepath(path)
-        return m.write(tail, *args, **kwargs)
-
+        return self.rootfsmodule.write(path, *args, **kwargs)
 
     def truncate(self, path, *args, **kwargs):
         path = path[1:]
-        (m, tail) = self.modulepath(path)
-        return m.truncate(tail, *args, **kwargs)
+        return self.rootfsmodule.truncate(path, *args, **kwargs)
 
 
 
